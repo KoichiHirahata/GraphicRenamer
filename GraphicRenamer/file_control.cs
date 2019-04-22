9cd5ebf90252fdae87d30dc8a8eaa286879c73ee
Jyo.S <saito@core-system.jp>
2019-04-22 19:02:39 +0900

    IDのチェックとフォルダ階層作成メソッド作成

--- GraphicRenamer/file_control.cs
+++ GraphicRenamer/file_control.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace GraphicRenamer
 {
@@ -76,6 +79,57 @@ namespace GraphicRenamer
             }
 
             return ret;
+        }
+
+        /// <summary>
+        /// IDの桁数と数字かどうかのチェック
+        /// 12桁以上
+        /// </summary>
+        /// <param name="_id">チェック対象のID</param>
+        /// <returns>有効なIDだったらtrue、無効なIDだったらfalse</returns>
+        public static bool CheckID(string _id)
+        {
+            //数字かどうかのチェック
+            var isNum = _id.All(char.IsDigit);
+            if (isNum == false)
+            {
+                return false;
+            }
+            //長さのチェック
+            var idLength = _id.Length;
+            if (idLength > 12)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 桁数÷4の余りの数だけIDの先頭から数字を取ってきて、その名前のフォルダを利用する（なければ作る）（例: 54321 だったら 5 ）
+        /// その下にその次の4桁の番号の名前のフォルダを利用する（なければ作る）（例: 54321 だったら 4321 ）
+        /// </summary>
+        /// <param name="_id">対象のID</param>
+        /// <returns></returns>
+        public static string MakeDirPath(string _id)
+        {
+            var digits = _id.Length;
+            if (digits < 4)
+            {
+                var ret = @"\" + digits.ToString() + @"\" + _id;
+                return ret;
+            }
+            else
+            {
+                List<string> pathList = new List<string>();
+                var temp = digits % 4;//余り
+                pathList.Add(digits.ToString());//桁数
+                if (temp != 0)
+                {
+                    pathList.Add(_id.Substring(0, temp));//余りの数だけ文字取得
+                }
+                pathList.AddRange(Regex.Split(_id.Substring(temp), @"(?<=\G.{4})(?!$)"));//4文字区切りでListにいれる
+                return @"\"+string.Join(@"\", pathList);
+            }
         }
     }
 }
---
+++ GraphicRenamerTests1/file_controlTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GraphicRenamer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicRenamer.Tests
+{
+    [TestClass()]
+    public class file_controlTests
+    {
+        [TestMethod()]
+        public void CheckIDTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
